d7570ac64af46e75eb9f2c4e0d41ea39199d1aa1
such <suchen9209@126.com>
2018-10-25 11:35:51 +0800

    增加memcached的缓存时间
    暂定为48小时，不知微信小程序的storage缓存时间

--- application/models/function/User_account_model.php
+++ application/models/function/User_account_model.php
@@ -15,7 +15,7 @@ class User_account_model extends CI_Model {
     private function save_info($parm){
         $this->load->driver('cache');
         foreach ($parm as $key => $value) {
-            $this->cache->memcached->save($key, $value);
+            $this->cache->memcached->save($key, $value, 60*60*48);
         }
     }
 
@@ -66,9 +66,6 @@ class User_account_model extends CI_Model {
 
         return $return_arr;
     }
-
-
-
     
 }
 ?>

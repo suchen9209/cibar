fb3c265f7584e07a1c86d9a8b7cb7f81424fd5e6
such <suchen9209@126.com>
2018-10-30 15:16:21 +0800

    去除while

--- application/controllers/bar/Message.php
+++ application/controllers/bar/Message.php
@@ -9,21 +9,18 @@ class Message extends CI_Controller {
         $this->load->driver('cache');
         $first = true;
 
-        while (true) {
-            $service_json_recent = $this->cache->memcached->get('service_recent');
-            $service_json = $this->cache->memcached->get('service');
+        $service_json_recent = $this->cache->memcached->get('service_recent');
+        $service_json = $this->cache->memcached->get('service');
 
-            if(($service_json_recent && $service_json_recent != $service_json) || $first){//和之前的信息有区别时发送信息 或首次打开
-                $first = false;
-                $this->cache->memcached->save('service_recent',$service_json);
-                echo "data: {$service_json}\n\n";
-                ob_flush();
-                flush();
+        if(($service_json_recent && $service_json_recent != $service_json) || $first){//和之前的信息有区别时发送信息 或首次打开
+            $first = false;
+            $this->cache->memcached->save('service_recent',$service_json);
+            echo "data: {$service_json}\n\n";
+            ob_flush();
+            flush();
 
-            }else{
+        }else{
                 //do nothing
-            }
-
         }
 
 	}

d328660600f3a04c2b13c4254e97cb0e4d81024c
such <suchen9209@126.com>
2018-10-30 15:06:20 +0800

    test

--- application/controllers/bar/Message.php
+++ application/controllers/bar/Message.php
@@ -8,18 +8,19 @@ class Message extends CI_Controller {
         header('Cache-Control: no-cache');
 
         $this->load->driver('cache');
-        $service_json = $this->cache->memcached->get('service');
-        echo "data: {$service_json}\n\n";
-        flush();
+        $first = true;
 
         while (1) {
             $service_json_recent = $this->cache->memcached->get('service_recent');
             $service_json = $this->cache->memcached->get('service');
 
-            if(($service_json_recent && $service_json_recent != $service_json)){//和之前的信息有区别时发送信息 或首次打开
+            if(($service_json_recent && $service_json_recent != $service_json) || $first){//和之前的信息有区别时发送信息 或首次打开
+                $first = false;
                 $this->cache->memcached->save('service_recent',$service_json);
                 echo "data: {$service_json}\n\n";
+                ob_flush();
                 flush();
+
             }else{
                 //do nothing
             }

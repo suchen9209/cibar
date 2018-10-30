6be84b90f529727cdb2377b85ec9d1e5e0482223
such <suchen9209@126.com>
2018-10-30 14:47:26 +0800

    \n\n为结束换行符

--- application/controllers/bar/Message.php
+++ application/controllers/bar/Message.php
@@ -11,7 +11,7 @@ class Message extends CI_Controller {
         $service_json = $this->cache->memcached->get('service');
 
         $time = date('r');
-        echo "data: The server time is: {$time} {$service_json}\n\n";
+        echo "data: {$service_json}\n\n";
         flush();
 
 	}

f2f352478196bf02575bf59d6e7997dee84f32bb
such <suchen9209@126.com>
2018-10-30 15:23:43 +0800

    增加header试下能否支撑while循环

--- application/controllers/bar/Message.php
+++ application/controllers/bar/Message.php
@@ -5,23 +5,28 @@ class Message extends CI_Controller {
 
 	public function index(){        
         header('Content-Type: text/event-stream');
+        header('Cache-Control: no-cache');
+        header('Connection: keep-alive');
+        header('X-Accel-Buffering: no');
 
         $this->load->driver('cache');
-        $first = true;
 
-        $service_json_recent = $this->cache->memcached->get('service_recent');
-        $service_json = $this->cache->memcached->get('service');
+        while (1) {
+            $service_json_recent = $this->cache->memcached->get('service_recent');
+            $service_json = $this->cache->memcached->get('service');
 
-        if(($service_json_recent && $service_json_recent != $service_json) || $first){//和之前的信息有区别时发送信息 或首次打开
-            $first = false;
-            $this->cache->memcached->save('service_recent',$service_json);
-            echo "data: {$service_json}\n\n";
-            ob_flush();
-            flush();
+            if($service_json_recent != $service_json){//和之前的信息有区别时发送信息 或首次打开
+                $this->cache->memcached->save('service_recent',$service_json,30);
+                echo "data: {$service_json}\n\n";
+                ob_flush();
+                flush();
+            }else{
+                    //do nothing
+            }
 
-        }else{
-                //do nothing
+            sleep(1);
         }
+        
 
 	}
 

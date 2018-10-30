f8dd63984db2900624c7bf2fd42f3e3d3d25d402
such <suchen9209@126.com>
2018-10-30 14:55:01 +0800

    测试
    增加自身循环，休息1s，在两次json不同时触发

--- application/controllers/bar/Message.php
+++ application/controllers/bar/Message.php
@@ -8,16 +8,31 @@ class Message extends CI_Controller {
         header('Cache-Control: no-cache');
 
         $this->load->driver('cache');
-        $service_json = $this->cache->memcached->get('service');
 
-        $time = date('r');
-        echo "data: {$service_json}\n\n";
-        flush();
+        while (1) {
+            $service_json_recent = $this->cache->memcached->get('service_recent');
+            $service_json = $this->cache->memcached->get('service');
+
+            if($service_json_recent && $service_json_recent != $service_json){//和之前的信息有区别时发送信息
+                $this->cache->memcached->save('service_recent',$service_json);
+                echo "data: {$service_json}\n\n";
+                flush();
+            }else{
+                //do nothing
+            }
+
+            sleep(1);
+        }
 
 	}
 
     public function html(){
         $this->load->view('bar/test');
+    }
+
+    public function random_service(){
+        $this->load->driver('cache');
+        $this->cache->memcached->save('service',mt_rand());
     }
 
 }

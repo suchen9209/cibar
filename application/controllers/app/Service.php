6041710f7f0051c12774534864d8d1e1237df995
such <suchen9209@126.com>
2018-10-30 16:52:04 +0800

    创建缓存类外移，方便后期处理

--- application/controllers/app/Service.php
+++ application/controllers/app/Service.php
@@ -8,21 +8,18 @@ class Service extends Test_Api_Controller {
 		//$session_name = 'user_id';
 		parent::__construct('app_rest');
 		$this->load->model('function/user_account_model','user_account');
-		$this->load->model('function/user_seat_model','user_seat');
+		$this->load->model('function/service_function_model','service_function');
 	}
 
 	public function index(){
 		$uid = $this->getUserId();
 		if($uid){
 			//呼叫服务
-			$seat_info = $this->user_seat->get_seat($uid);
-			$this->load->driver('cache');
-			$service_json = $this->cache->memcached->get('service');
-			$service = json_decode($service_json,true);
-			$service[]= $seat_info;
-			$this->cache->memcached->save('service',json_encode($service),3600*24);
-
-			$this->response($this->getResponseData(parent::HTTP_OK, '呼叫成功'), parent::HTTP_OK);
+			if($this->service_function->call_service($uid)){
+				$this->response($this->getResponseData(parent::HTTP_OK, '呼叫成功'), parent::HTTP_OK);
+			}else{
+				$this->response($this->getResponseData(parent::HTTP_OK, '呼叫失败，请重试'), parent::HTTP_OK);
+			}
 		}else{
 			$this->response($this->getResponseData(parent::HTTP_BAD_REQUEST, '登录信息失效，请重新登录'), parent::HTTP_OK);
 		}
---
+++ application/models/function/Service_function_model.php
@@ -0,0 +1,50 @@
+<?php 
+class Service_function_model extends CI_Model {
+
+    /*
+        绑定用户和账户
+    */
+    private $service_memcached_name = 'service';
+
+    public function __construct()
+    {
+        $this->load->database();
+        $this->load->model('function/user_seat_model','user_seat');
+        $this->load->driver('cache');
+    }
+
+    public function call_service($uid){
+        $seat_info = $this->user_seat->get_seat($uid);
+        $service_json = $this->cache->memcached->get($this->$service_memcached_name);
+        if($service_json){
+            $service = json_decode($service_json,true);
+            $service[]= $seat_info; 
+        }else{
+            $service = array($seat_info);
+        }
+        
+        return $this->cache->memcached->save($this->$service_memcached_name,json_encode($service),3600*24);
+    }
+
+    public function remove_service($uid){
+        $seat_info = $this->user_seat->get_seat($uid);
+        $service_json = $this->cache->memcached->get($this->$service_memcached_name);
+        $service = json_decode($service_json,true);
+
+        $key = array_search($seat_info, $service);
+        if ($key !== false){
+            array_splice($service, $key, 1);
+        }
+        return $this->cache->memcached->save($this->$service_memcached_name,json_encode($service),3600*24);
+    }
+
+
+    public function get_all_service(){
+        $service_json = $this->cache->memcached->get($this->$service_memcached_name);
+
+        return $service_json;
+    }
+
+
+}
+?>

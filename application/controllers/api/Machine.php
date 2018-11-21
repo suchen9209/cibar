e90af986886ed62aad6bbd5a2ff648754138b986
such <suchen9209@126.com>
2018-11-21 15:04:40 +0800

    Update Machine.php

--- application/controllers/api/Machine.php
+++ application/controllers/api/Machine.php
@@ -101,7 +101,7 @@ class Machine extends Admin_Api_Controller {
 
                 if($last_p = $this->peripheral_last->get_last_by_uid($uid)){
                     $pjson = $last_p[0]['pid'];
-                    $pdata = json_decode($pjson);
+                    $pdata = json_decode($pjson,true);
                     foreach ($pdata as $key => $value) {
                         $this->peripheral_num->in($value['id']);
                     }

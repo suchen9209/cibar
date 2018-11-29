a48ea603ceb70d15fbc391f4d56380663e4b9053
such <suchen9209@126.com>
2018-11-29 15:17:55 +0800

    更新至正式环境

--- application/models/function/Send_wokerman_model.php
+++ application/models/function/Send_wokerman_model.php
@@ -4,7 +4,8 @@ class Send_wokerman_model extends CI_Model {
 
 
     public function send($msg){
-        $client = stream_socket_client('tcp://39.107.64.199:2347', $errno, $errmsg, 1);
+        //$client = stream_socket_client('tcp://39.107.64.199:2347', $errno, $errmsg, 1);
+        $client = stream_socket_client('tcp://127.0.0.1:10445', $errno, $errmsg, 1);
     	fwrite($client, $msg."\n");
     }
 

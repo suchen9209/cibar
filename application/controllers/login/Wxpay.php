35565c72c7a8b6b611b0a979d7685baec8e6a259
such <suchen9209@126.com>
2018-12-06 17:46:32 +0800

    Update Wxpay.php

--- application/controllers/login/Wxpay.php
+++ application/controllers/login/Wxpay.php
@@ -90,58 +90,21 @@ class Wxpay extends Weixin {
     
     }
 
-    public function test(){
-
-         $http = new URL();
-         $arr = array(
-            'appid' => '123123',
-            'wx'    =>  '234234'
-         );
-
-         $str = '<xml>
-  <appid><![CDATA[wx2421b1c4370ec43b]]></appid>
-  <attach><![CDATA[支付测试]]></attach>
-  <bank_type><![CDATA[CFT]]></bank_type>
-  <fee_type><![CDATA[CNY]]></fee_type>
-  <is_subscribe><![CDATA[Y]]></is_subscribe>
-  <mch_id><![CDATA[10000100]]></mch_id>
-  <nonce_str><![CDATA[5d2b6c2a8db53831f7eda20af46e531c]]></nonce_str>
-  <openid><![CDATA[oUpF8uMEb4qRXf22hE3X68TekukE]]></openid>
-  <out_trade_no><![CDATA[1409811653]]></out_trade_no>
-  <result_code><![CDATA[SUCCESS]]></result_code>
-  <return_code><![CDATA[SUCCESS]]></return_code>
-  <sign><![CDATA[B552ED6B279343CB493C5DD0D78AB241]]></sign>
-  <sub_mch_id><![CDATA[10000100]]></sub_mch_id>
-  <time_end><![CDATA[20140903131540]]></time_end>
-  <total_fee>1</total_fee>
-<coupon_fee><![CDATA[10]]></coupon_fee>
-<coupon_count><![CDATA[1]]></coupon_count>
-<coupon_type><![CDATA[CASH]]></coupon_type>
-<coupon_id><![CDATA[10000]]></coupon_id>
-<coupon_fee><![CDATA[100]]></coupon_fee>
-  <trade_type><![CDATA[JSAPI]]></trade_type>
-  <transaction_id><![CDATA[1004400740201409030005092168]]></transaction_id>
-</xml>';
-/*        $res_xml = $http->post('https://pay.imbatv.cn/login/wxpay/back',$str);*/
-$res_xml = $http->post('https://pay.imbatv.cn/login/wxpay/back','<xml><appid><![CDATA[wx2421b1c4370ec43b]]></appid><attach><![CDATA[支付测试]]></attach><bank_type><![CDATA[CFT]]></bank_type><fee_type><![CDATA[CNY]]></fee_type><is_subscribe><![CDATA[Y]]></is_subscribe><mch_id><![CDATA[10000100]]></mch_id><nonce_str><![CDATA[5d2b6c2a8db53831f7eda20af46e531c]]></nonce_str><openid><![CDATA[oUpF8uMEb4qRXf22hE3X68TekukE]]></openid><out_trade_no><![CDATA[1409811653]]></out_trade_no><result_code><![CDATA[SUCCESS]]></result_code><return_code><![CDATA[SUCCESS]]></return_code><sign><![CDATA[B552ED6B279343CB493C5DD0D78AB241]]></sign><sub_mch_id><![CDATA[10000100]]></sub_mch_id><time_end><![CDATA[20140903131540]]></time_end><total_fee>1</total_fee><coupon_fee><![CDATA[10]]></coupon_fee><coupon_count><![CDATA[1]]></coupon_count><coupon_type><![CDATA[CASH]]></coupon_type><coupon_id><![CDATA[10000]]></coupon_id><coupon_fee><![CDATA[100]]></coupon_fee>  <trade_type><![CDATA[JSAPI]]></trade_type><transaction_id><![CDATA[1004400740201409030005092168]]></transaction_id></xml>');
-
-        //header('Content-Type:application/xml');
-        echo $res_xml;
-    }
-
     public function back(){
         $xmldata = file_get_contents("php://input");
-        var_dump($xmldata);
         $obj = simplexml_load_string($res_xml, 'SimpleXMLElement', LIBXML_NOCDATA);
         $data = json_decode(json_encode($obj), true);
-        var_dump($xmldata);
-        die;
 
         if($data['return_code'] == 'SUCCESS'){
 
             //验证签名，保证数据为服务器传递过来的数据
             $receive_sign = $data['sign'];
             unset($data['sign']);
+            foreach ($data as $key => $value) {
+                if(!$value){
+                    unset($data[$key]);
+                }
+            }
             $calculate_sign = $this->array_to_str_special($data);
 
             if($calculate_sign == $receive_sign){

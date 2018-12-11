769231fe8ee341aff36409208e8776e6ec0d29a7
such <suchen9209@126.com>
2018-12-11 14:24:05 +0800

    Update Machine.php

--- application/controllers/tool/Machine.php
+++ application/controllers/tool/Machine.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Machine extends Admin_Controller {
+class Machine extends Admin_Api_Controller {
 
     public function __construct(){
         parent::__construct();

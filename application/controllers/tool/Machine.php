86f56545f0a1c6d7198d75bef9cc16741967df96
such <suchen9209@126.com>
2018-12-11 14:48:07 +0800

    Update Machine.php

--- application/controllers/tool/Machine.php
+++ application/controllers/tool/Machine.php
@@ -30,7 +30,12 @@ class Machine extends Admin_Api_Controller {
     public function config_info(){
 
         $return_arr['status_list'] = $this->config->item('machine_hardware_status');
-        $return_arr['type_list'] = $this->config->item('machine_type');
+        $machine_type = $this->config->item('machine_type');
+        $return = array();
+        foreach ($machine_type as $key => $value) {
+            $return []= array('id'=>$key,'name'=>$value);
+        }
+        $return_arr['type_list'] = $return;
 
         $this->response($this->getResponseData(parent::HTTP_OK, '机器类型和硬件状态',$return_arr), parent::HTTP_OK);
     }
@@ -98,141 +103,6 @@ class Machine extends Admin_Api_Controller {
         }
 
     }
-
-    public function insert_test(){
-        die;
-        for ($i=1; $i <= 18; $i++) { 
-            $parm['machine_name'] = 'S'.str_pad($i,3,"0",STR_PAD_LEFT);
-            $parm['ip'] = '192.168.15.'.$i;
-            $parm['type'] = 1;
-            $parm['position'] = $i;
-            $parm['box_id'] = '散座';
-            
-            $this->db->trans_start();
-            $machine_id = $this->machine->insert($parm);
-
-            $info_parm = array('machine_id'=>$machine_id,'machine_info'=>'预留位置','ip'=>$parm['ip'],'mac'=>'mac地址');
-            $this->machine_info->insert($info_parm);
-
-            $active_parm = array('mid'=>$machine_id,'state'=>1);
-            $this->active_status->insert($active_parm);
-
-            if($this->db->trans_status() === FALSE){
-                $this->db->trans_rollback();   //增加失败，回滚
-            }else{
-                $this->db->trans_complete();
-            }
-        }
-
-        $num = 1;
-        for ($i=1; $i <= 30; $i++) { 
-            for ($k=1; $k <= 5; $k++) { 
-                $parm['machine_name'] = 'FB'.str_pad($num,3,"0",STR_PAD_LEFT);
-                $parm['ip'] = '192.168.222.'.$num;
-                $parm['type'] = 2;
-                $parm['position'] = $k;
-                $parm['box_id'] = '5人包厢'.$i;
-                
-                $this->db->trans_start();
-                $machine_id = $this->machine->insert($parm);
-
-                $info_parm = array('machine_id'=>$machine_id,'machine_info'=>'预留位置','ip'=>$parm['ip'],'mac'=>'mac地址');
-                $this->machine_info->insert($info_parm);
-
-                $active_parm = array('mid'=>$machine_id,'state'=>1);
-                $this->active_status->insert($active_parm);
-
-                if($this->db->trans_status() === FALSE){
-                    $this->db->trans_rollback();   //增加失败，回滚
-                }else{
-                    $this->db->trans_complete();
-                }
-                $num ++;
-            }
-            
-        }
-
-        $num = 1;
-        for ($i=1; $i <= 3; $i++) { 
-            for ($k=1; $k <= 6; $k++) { 
-                $parm['machine_name'] = 'SB'.str_pad($num,3,"0",STR_PAD_LEFT);
-                $parm['ip'] = '192.168.244.'.$num;
-                $parm['type'] = 4;
-                $parm['position'] = $k;
-                $parm['box_id'] = '6人包厢'.$i;
-                
-                $this->db->trans_start();
-                $machine_id = $this->machine->insert($parm);
-
-                $info_parm = array('machine_id'=>$machine_id,'machine_info'=>'预留位置','ip'=>$parm['ip'],'mac'=>'mac地址');
-                $this->machine_info->insert($info_parm);
-
-                $active_parm = array('mid'=>$machine_id,'state'=>1);
-                $this->active_status->insert($active_parm);
-
-                if($this->db->trans_status() === FALSE){
-                    $this->db->trans_rollback();   //增加失败，回滚
-                }else{
-                    $this->db->trans_complete();
-                }
-                $num ++;
-            }
-            
-        }
-
-        $num = 1;
-        for ($i=1; $i <= 2; $i++) { 
-            for ($k=1; $k <= 10; $k++) { 
-                $parm['machine_name'] = 'TB'.str_pad($num,3,"0",STR_PAD_LEFT);
-                $parm['ip'] = '192.168.55.'.$num;
-                $parm['type'] = 4;
-                $parm['position'] = $k;
-                $parm['box_id'] = '10人包厢'.$i;
-                
-                $this->db->trans_start();
-                $machine_id = $this->machine->insert($parm);
-
-                $info_parm = array('machine_id'=>$machine_id,'machine_info'=>'预留位置','ip'=>$parm['ip'],'mac'=>'mac地址');
-                $this->machine_info->insert($info_parm);
-
-                $active_parm = array('mid'=>$machine_id,'state'=>1);
-                $this->active_status->insert($active_parm);
-
-                if($this->db->trans_status() === FALSE){
-                    $this->db->trans_rollback();   //增加失败，回滚
-                }else{
-                    $this->db->trans_complete();
-                }
-                $num ++;
-            }
-            
-        }
-
-        for ($k=1; $k <= 20; $k++) { 
-            $parm['machine_name'] = 'TTB'.str_pad($k,3,"0",STR_PAD_LEFT);
-            $parm['ip'] = '192.168.22.'.$k;
-            $parm['type'] = 4;
-            $parm['position'] = $k;
-            $parm['box_id'] = '20人包厢';
-            
-            $this->db->trans_start();
-            $machine_id = $this->machine->insert($parm);
-
-            $info_parm = array('machine_id'=>$machine_id,'machine_info'=>'预留位置','ip'=>$parm['ip'],'mac'=>'mac地址');
-            $this->machine_info->insert($info_parm);
-
-            $active_parm = array('mid'=>$machine_id,'state'=>1);
-            $this->active_status->insert($active_parm);
-
-            if($this->db->trans_status() === FALSE){
-                $this->db->trans_rollback();   //增加失败，回滚
-            }else{
-                $this->db->trans_complete();
-            }
-            $num ++;
-        }
-    }
-
 
 
 }

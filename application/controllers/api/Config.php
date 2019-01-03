37a935fc4a385914c64addceb55509c3888632a5
such <suchen9209@126.com>
2019-01-03 17:42:19 +0800

    Update Config.php

--- application/controllers/api/Config.php
+++ application/controllers/api/Config.php
@@ -5,6 +5,7 @@ class Config extends Admin_Api_Controller {
 
 	public function __construct(){
 		parent::__construct();
+        $this->load->model('coupon_model','coupon');
 	}
 
 	public function index()
@@ -18,6 +19,10 @@ class Config extends Admin_Api_Controller {
             $tmp_type []= $tmp;
         }
         $return_data['pay_type'] = $tmp_type;
+
+        //当前可以用优惠券
+        $return_data['coupon_list'] = $this->coupon->get_list(0,100,array('state'=>1));
         $this->response($this->getResponseData(parent::HTTP_OK, '支付类型', $return_data), parent::HTTP_OK);
 	}
+
 }

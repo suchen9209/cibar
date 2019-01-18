d3f265f2afee011c62b0263275a21a4983bdcfd4
such <suchen9209@126.com>
2019-01-18 15:38:32 +0800

    Update Test_cron.php

--- application/controllers/bot/Test_cron.php
+++ application/controllers/bot/Test_cron.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+require_once dirname(__FILE__).'/Deduct.php';
 class Test_cron extends Ci_Controller {
 
 	public function __construct(){
@@ -11,8 +12,15 @@ class Test_cron extends Ci_Controller {
 	}
 
 	public function index(){
-
+		$test = new Deduct();
 		$uids = $this->active_status->get_all_live_user_id();
+		foreach ($uids as $key => $value) {
+			$_GET['uid'] = $value['uid'];
+			$_GET['utime'] = 180;
+
+			$test->index();
+		}
+/*		$uids = $this->active_status->get_all_live_user_id();
 		foreach ($uids as $key => $value) {
 			$temp_uid = $value['uid'];
 			$url = "https://pay.imbatv.cn/bot/deduct/index?uid=".$temp_uid."&utime=180";
@@ -22,7 +30,7 @@ class Test_cron extends Ci_Controller {
 		    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true) ; // 在启用 CURLOPT_RETURNTRANSFER 时候将获取数据返回    
 		    $r = curl_exec($ch);  
 		    curl_close($ch);  
-		}
+		}*/
 	}
 
 

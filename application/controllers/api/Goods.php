7a2c8be93f41f3d4ed50e77db9bcdf224af4c992
such <suchen9209@126.com>
2019-01-22 12:34:09 +0800

    改为计算使用优惠券后的价格

--- application/controllers/api/Goods.php
+++ application/controllers/api/Goods.php
@@ -36,26 +36,59 @@ class Goods extends Admin_Api_Controller {
         
     }
 
-    //不用了 20190118
     public function calculate_discount(){
         $list_json = $this->input->post_get('cartList');
         $uid = $this->input->get_post('user_id')?$this->input->get_post('user_id'):0;
+        $user_coupon_id = $this->input->get_post('user_coupon_id')?$this->input->get_post('user_coupon_id'):0;
 
-        $level = $this->user_account->get_member_level($uid);
-        $discount = $this->config->item('discount_level')[$level];
+/*        $list_json = '[{"id":4,"name":"红牛","price":5.00,"quantity":2,"type":2},{"id":3,"name":"巴黎水","price":8.00,"quantity":2,"type":2},{"id":2,"name":"依云","price":4.00,"quantity":2,"type":2},{"id":1,"name":"可乐","price":3.00,"quantity":2,"type":2}]';
+        $user_coupon_id= 27;
+        $uid = 57;*/
+        $user_coupon_info = $this->user_coupon->get_info($user_coupon_id);
+        $coupon_info = $this->coupon->get_info($user_coupon_info->cid);
+        $discount_good_ids = $coupon_info->good_ids;
+        $discount_good_ids = ','.$discount_good_ids.',';
 
         $list = json_decode($list_json);
+        $id_name = array();
 
         $total_money = 0;
-        foreach ($list as $key => $value) {//计入消费记录
+        $need_discount_goods = array();
+        foreach ($list as $key => $value) {
             $tmp_number = $value->quantity;
             $tmp_price = $value->price;
-            $tmp_discount_price = round($value->price * $discount,2);
+            $total_money += $tmp_number * $tmp_price;
+            if(strpos($discount_good_ids, ','.$value->id.',') !== false){
+                for ($i=0; $i < $value->quantity; $i++) { 
+                    $need_discount_goods []= array('id'=>$value->id,'price'=>$value->price,'name'=>$value->name);
+                }
+            }
 
-            $total_money += $tmp_number * $tmp_discount_price;
+            $id_name[$value->id] = $value->name;
+
+        }
+        array_multisort(array_column($need_discount_goods, 'price'),SORT_DESC,SORT_NUMERIC,$need_discount_goods);
+        $reduce_money = 0;
+        $reduce_good_list = array();
+        for ($j=0; $j < $coupon_info->num; $j++) { 
+            $reduce_money += (1-$coupon_info->discount)*$need_discount_goods[$j]['price'];
+            if($reduce_good_list[$need_discount_goods[$j]['id']]){
+                $reduce_good_list[$need_discount_goods[$j]['id']] ++;
+            }else{
+                $reduce_good_list[$need_discount_goods[$j]['id']] = 1;
+            }
         }
 
-        $this->response($this->getResponseData(parent::HTTP_OK, '折扣后金额', $total_money), parent::HTTP_OK);
+        foreach ($reduce_good_list as $key => $value) {
+            $return_data['reduce_good'] []= array('name' => $id_name[$key],'quantity'=>$value);
+            # code...
+        }
+
+        $return_data['total_money'] = $total_money;//总价
+        $return_data['discount_money'] = $total_money - $reduce_money;
+
+
+        $this->response($this->getResponseData(parent::HTTP_OK, '结算信息', $return_data), parent::HTTP_OK);
 
     }
 

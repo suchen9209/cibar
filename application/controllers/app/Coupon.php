f145bc51bfd05f92b1c6242855e0f58669155179
such <suchen9209@126.com>
2019-01-23 14:05:40 +0800

    小程序优惠券

--- application/controllers/app/Coupon.php
+++ application/controllers/app/Coupon.php
@@ -26,10 +26,16 @@ class Coupon extends App_Api_Controller {
     public function coupon_list(){
         $uid = $this->getUserId();
         $type = $this->input->get_post('type') ? $this->input->get_post('type') : 0;
+        $outdate = $this->input->get_post('outdate') ? $this->input->get_post('outdate') : 0;
         if($uid){
-            $data = $this->user_coupon->get_can_use_by_uid_type($uid,$type);
-
-            $this->response($this->getResponseData(parent::HTTP_OK,'优惠券列表',$data), parent::HTTP_OK);
+            if($outdate > 0){
+                $data = $this->user_coupon->get_cannot_use_by_uid_type($uid,$type);
+                $this->response($this->getResponseData(parent::HTTP_OK,'不可用优惠券列表',$data), parent::HTTP_OK);
+            }else{
+                $data = $this->user_coupon->get_can_use_by_uid_type($uid,$type);
+                $this->response($this->getResponseData(parent::HTTP_OK,'可用优惠券列表',$data), parent::HTTP_OK);    
+            }
+            
         }else{
             $this->response($this->getResponseData(parent::HTTP_BAD_REQUEST, '登录信息过期，请重新登录'), parent::HTTP_OK);
         }

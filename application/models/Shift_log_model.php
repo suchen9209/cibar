8ea8ad131bee657d140091958dd43b0a9d506eb5
such <suchen9209@126.com>
2019-04-28 18:36:17 +0800

    暂时不区分交接班时日期

--- application/models/Shift_log_model.php
+++ application/models/Shift_log_model.php
@@ -9,9 +9,9 @@ class Shift_log_model extends CI_Model {
 
     public function get_last_time($ouid = 0){
         $this->db->select('time');
-        if($ouid != 0){
+/*        if($ouid != 0){
             $this->db->where('ouid',$ouid);
-        }
+        }*/
         $this->db->order_by('time','DESC');
         $this->db->limit(1,0);
         $query = $this->db->get($this->table_name);

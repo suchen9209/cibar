3b99dc4bf563fbc91013625ca0629a65f9dd8a27
such <suchen9209@126.com>
2019-05-06 11:56:13 +0800

    Update User.php

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -344,7 +344,7 @@ class User extends Admin_Api_Controller {
         //$this->response($this->getResponseData(parent::HTTP_OK, '充值记录', $return_data), parent::HTTP_OK);
     }
 
-        //获取在线用户的信息
+    //获取在线用户的信息
     public function get_active_user_info(){
         $uid = $this->input->get_post('user_id') ? $this->input->get_post('user_id') : 0;
         if($uid > 0){
@@ -352,6 +352,14 @@ class User extends Admin_Api_Controller {
             $login_info = $this->log_login->get_last_login_info($uid);
             $user_info['opentime'] = $login_info->time;
             $user_info['duration'] = time() - $login_info->time;
+
+            $deduct_info = $this->log_deduct_money->get_total_info($value['uid']);
+            if($deduct_info){
+                $user_info['cost'] = $deduct_info['total_money'];
+            }else{
+                $user_info['cost'] = 0;
+            }
+
             $this->response($this->getResponseData(parent::HTTP_OK, '用户信息', $user_info), parent::HTTP_OK);
         }else{
             $this->response($this->getResponseData(parent::HTTP_BAD_REQUEST, '参数错误', 'nothing'), parent::HTTP_OK);

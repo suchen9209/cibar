bbe85c408af842d1cc8173ae407c8ec76970df77
such <suchen9209@126.com>
2019-05-29 10:56:24 +0800

    更新为正式需session验证

--- application/config/admin_rest.php
+++ application/config/admin_rest.php
@@ -87,7 +87,7 @@ $config['rest_realm'] = 'REST API';
 |           authorization key
 |
 */
-$config['rest_auth'] = FALSE;
+$config['rest_auth'] = 'session';
 
 /*
 |--------------------------------------------------------------------------

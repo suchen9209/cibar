bcb9de7d49b14129d63f42c99247585774bf5f7e
such <suchen9209@126.com>
2019-11-28 14:55:09 +0800

    Update Index.php

--- application/controllers/app/Index.php
+++ application/controllers/app/Index.php
@@ -22,6 +22,20 @@ class Index extends App_Api_Controller {
 		
 	}
 
+	public function is_bind_phone(){
+		$uid = $this->getUserId();
+		if($uid){
+			$user_info = $this->user_account->get_user_info($uid);
+			if($user_info['phone']){
+				$this->response($this->getResponseData(parent::HTTP_OK, '已绑定', 1), parent::HTTP_OK);
+			}else{
+				$this->response($this->getResponseData(parent::HTTP_OK, '未绑定', 2), parent::HTTP_OK);
+			}			
+		}else{
+			$this->response($this->getResponseData(parent::HTTP_BAD_REQUEST, '用户信息', 'nothing'), parent::HTTP_OK);
+		}
+	}
+
 	public function check_user_state(){
 		$this->response($this->getResponseData(parent::HTTP_OK, '是否允许', 1), parent::HTTP_OK);
 	}
